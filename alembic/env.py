405bce127d377ed14af684fd721d1007a2ada234
Raktim <reachraktim@gmail.com>
2026-06-03 18:51:32 +0200

    setup pg + setup alembic with vector extension

--- alembic/env.py
+++ alembic/env.py
@@ -18,7 +18,6 @@ config = context.config
 ########## Settings for SCOUTR ####################
 settings = get_settings()
 config.set_main_option("sqlalchemy.url", settings.database_url)
-target_metadata = Base.metadata
 ########################################
 
 # Interpret the config file for Python logging.
@@ -30,7 +29,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
